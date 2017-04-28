4cd482214c172e8f4a0560f12650ed71c3ea3a79
H0K4 <h0k4ch4n@gmail.com>
2017-04-28 13:18:07 +0200

    Improved the categories theme and remove frameset and RSS theme
    added improvements from lainchan's uboachan categories theme.  New
    categories theme is still in beta.  I also deleted frameset and RSS
    theme since categories is an exact copy of frameset and the RSS theme
    whas not working and barely took any content other then the title of a
    thread.

--- templates/themes/categories/info.php
+++ templates/themes/categories/info.php
@@ -5,16 +5,16 @@
 	$theme['name'] = 'Categories';
 	// Description (you can use Tinyboard markup here)
 	$theme['description'] = 
-'Group-ordered, category-aware modification of the Frameset theme, with removable sidebar frame.
+'Uboachan modification of the Categories theme.
 
-Requires $config[\'categories\'].';
-	$theme['version'] = 'v0.3';
+Requires $config[\'boards\'] and $config[\'categories\'].';
+	$theme['version'] = 'v0.2.1';
 	
 	// Theme configuration	
 	$theme['config'] = Array();
 	
 	$theme['config'][] = Array(
-		'title' => 'Site title',
+		'title' => 'Title',
 		'name' => 'title',
 		'type' => 'text'
 	);
@@ -22,43 +22,18 @@ Requires $config[\'categories\'].';
 	$theme['config'][] = Array(
 		'title' => 'Slogan',
 		'name' => 'subtitle',
-		'type' => 'text',
-		'comment' => '(optional)'
-	);
-	
-	$theme['config'][] = Array(
-		'title' => 'Main HTML file',
-		'name' => 'file_main',
-		'type' => 'text',
-		'default' => $config['file_index'],
-		'comment' => '(eg. "index.html")'
-	);
-	
-	$theme['config'][] = Array(
-		'title' => 'Sidebar file',
-		'name' => 'file_sidebar',
-		'type' => 'text',
-		'default' => 'sidebar.html',
-		'comment' => '(eg. "sidebar.html")'
-	);
-	
-	$theme['config'][] = Array(
-		'title' => 'News file',
-		'name' => 'file_news',
-		'type' => 'text',
-		'default' => 'news.html',
-		'comment' => '(eg. "news.html")'
+		'type' => 'text'
 	);
 	
 	// Unique function name for building everything
 	$theme['build_function'] = 'categories_build';
-	$theme['install_callback'] = 'categories_install';
 	
-	if (!function_exists('categories_install')) {
+	$theme['install_callback'] = 'categories_install';
+	if(!function_exists('categories_install')) {
 		function categories_install($settings) {
 			global $config;
 			
-			if (!isset($config['categories'])) {
+			if(!isset($config['boards']) || !isset($config['categories'])) {
 				return Array(false, '<h2>Prerequisites not met!</h2>' . 
 					'This theme requires $config[\'boards\'] and $config[\'categories\'] to be set.');
 			}
