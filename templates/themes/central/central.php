101f4bc7568cc4617e0ecf73af1118de0af3ae50
H0K4 <h0k4ch4n@gmail.com>
2017-06-10 04:32:16 +0200

    updated central.php theme

--- templates/themes/central/central.php
+++ templates/themes/central/central.php
@@ -124,7 +124,6 @@
 	
 	<!-- Latest Post titles -->						
 	<div class="box-wrap-board">
-	<h2>Latest Posts</h2>
 		<ul>
 			{% for post in central_posts %}
 				<li>
@@ -135,11 +134,53 @@
 				</li>
 			{% endfor %}
 		</ul>
-	</div>		
+	</div>	
+
+	<!-- Useful Links Remove or add the ones you like -->			
+	 <div class="box linkage">
+		 <ul>
+			<li><a href="https://github.com/horija/hokachan" target="_blank">H0K4CH4N Script</a></li>
+		 	<li><a href="http://www.torproject.org/" target="_blank">Torproject</a></li>
+		 	<li><a href="http://engine.vichan.net" target="_blank">Vichan</a></li>
+		 	<li><a href="http://duckduckgo.com" target="_blank">Duck Duck Go</a></li>
+			<li><a href="http://thechangingmirror.com/phpbb/" target="_blank">The Changing Mirror</a></li>
+			<li><a href="http://literotica.com" target="_blank">Literotica</a></li>
+			<li><a href="http://g.e-hentai.org" target="_blank">E-hentai</a></li>
+			<li><a href="http://cyoc.net" target="_blank">Choose Your Own Change</a></li>
+			<li><a href="http://arania.kamiki.net" target="_blank">Arania</a></li>
+			<li><a href="http://e621.net" target="_blank">E621</a></li>
+			<li><a href="https://infowars.com" target="_blank">InfoWars</a></li>
+			<li><a href="https://zerohedge.com" target="_blank">Zero Hedge</a></li>
+			<li><a href="http://breitbart.com" target="_blank">Breitbart</a></li>
+			<li><a href="http://kekistan.com/" target="_blank">Kekistan</a></li>
+			<li><a href="http://fantasyfeeder.com/home" target="_blank">Fantasy Feeder</a></li>
+			<li><a href="http://theralphretort.com/" target="_blank">The Ralph Retort</a></li>
+			<li><a href="http://www.dimensionsmagazine.com/forums/" target="_blank">Dimensions</a></li>
+			<li><a href="http://8ch.net" target="_blank">8chan</a></li>
+			<li><a href="http://gab.ai" target="_blank">Gab</a></li>
+			<li><a href="http://liveleak.com" target="_blank">Liveleak</a></li>
+			<li><a href="http://www.hentai-foundry.com/" target="_blank">Hentai Foundry</a></li>
+			<li><a href="https://hentaihaven.org" target="_blank">Hentai Haven</a></li>
+			<li><a href="https://vid.me/" target="_blank"> Vidme</a></li>
+			<li><a href="https://www.minds.com" target="_blank">Minds</a></li>
+			<li><a href="https://encyclopediadramatica.rs/Main_Page" target="_blank">ED</a></li>
+			<li><a href="https://www.deviantart.com" target="_blank">Deviantart</a></li>
+			<li><a href="https://www.pixiv.net" target="_blank">Pixiv</a></li>
+			<li><a href="https://www.sofurry.com/" target="_blank">SoFurry</a></li>
+			<li><a href="https://rule34.xxx/" target="_blank">Rule 34</a></li>
+			<li><a href="https://www.friendsofscience.org/" target="_blank">Friends of Science</a></li>
+			<li><a href="https://www.loudtronix.co/" target="_blank">Loudtronix</a></li>
+			<li><a href="https://www.imgur.com/" target="_blank">IMGUR</a></li>
+			<li><a href="https://proxybay.one/" target="_blank">Pirate Bay Proxies</a></li>
+			<li><a href="https://www.reddit.com/r/CrackWatch/" target="_blank">Game Cracks Updates</a></li>
+			<li><a href="http://blackpigeonspeaks.com/" target="_blank">Black Pigeon Speaks</a></li>
+			<li><a href="http://www.opera.com/" target="_blank">Opera</a></li>
+			</ul>
+			<br style="clear:both" /> <!-- So dirty! -->
+		</div>
 		
 	<!-- Statistics -->
 	<div class="box-wrap-board">
-	<h2>Stats</h2>
 		<ul>
 			<li>Total posts: {{ stats.total_posts }}</li>
 			<li>Unique posters: {{ stats.unique_posters }}</li>
